0dc14637cefc599cf6f0afb19ef99936d7834e6e
manhee <angara99@gmail.com>
2023-06-10 14:03:05 -0700

    Added anothe project page

--- pages/photo/projects/[slug].tsx
+++ pages/photo/projects/[slug].tsx
@@ -13,8 +13,10 @@ import NextLink from 'components/reuseable/links/NextLink';
 import ProjectDetailsContent from 'components/common/ProjectDetailsContent';
 import ProjectDetailsNavigation from 'components/common/ProjectDetailsNavigation';
 import Button from 'components/elements/NavbarButton';
+
+import { ProjectCard1 } from 'components/reuseable/project-cards';
 // -------- data -------- //
-import { gallery } from 'data/project';
+import { gallery, projectList1 } from 'data/project';
 import { company } from 'data/company-info';
 
 const ProjectDetails: NextPage = () => {
@@ -42,80 +44,33 @@ const ProjectDetails: NextPage = () => {
       </header>
 
       <main className="content-wrapper">
-        <section
-          className="wrapper image-wrapper bg-image bg-cover bg-height"
-          style={{ backgroundImage: 'url(/img/photos/pp23.jpg)' }}
-        />
-
-        <section className="wrapper bg-light wrapper-border">
-          <div className="container py-14 py-md-16">
-            {/* ========== details section ========== */}
-            <ProjectDetailsContent
-              title="Memphis Brand Design"
-              titleClass="display-4 mb-4"
-              contentRowClass="row gx-0 mb-12"
-            />
-
-            {/* ========== gallery section ========== */}
-
-            <div className="row gy-6 mb-12">
-              {gallery.gallery1.map(({ width, height, id, url, className, fullUrl }) => (
-                <div className={className} key={id}>
-                  <figure className="hover-scale rounded cursor-dark">
-                    <a href={fullUrl} data-glightbox data-gallery="project-4">
-                      <Image
-                        width={width}
-                        height={height}
-                        src={url}
-                        alt="demo"
-                        style={{ width: '100%', height: 'auto' }}
-                      />
-                    </a>
-                  </figure>
+        {/* ========== title section ========== */}
+        <section className="section-frame overflow-hidden">
+          <div className="wrapper bg-gray">
+            <div className="container py-13 py-md-17 text-center">
+              <div className="row">
+                <div className="col-lg-10 col-xxl-8 mx-auto">
+                  <h1 className="display-1 mb-1">Check out some of our awesome projects with creative ideas.</h1>
                 </div>
-              ))}
-            </div>
-
-            <div className="row mb-12">
-              <div className="col-lg-10 mx-auto">
-                <h2 className="display-6 mb-4">About the Project</h2>
-                <p>
-                  Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante
-                  venenatis. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum.
-                </p>
-                <p className="mb-0">
-                  Donec id elit non mi porta gravida at eget metus. Cras mattis consectetur purus sit amet fermentum.
-                  Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Aenean eu leo
-                  quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at
-                  lobortis. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
-                </p>
               </div>
-            </div>
-
-            {/* ========== gallery section ========== */}
-            <div className="row gy-6">
-              {gallery.gallery2.map(({ width, height, id, url, className, fullUrl }) => (
-                <div className={className} key={id}>
-                  <figure className="hover-scale rounded cursor-dark">
-                    <a href={fullUrl} data-glightbox data-gallery="project-4">
-                      <Image
-                        width={width}
-                        height={height}
-                        src={url}
-                        alt="demo"
-                        style={{ width: '100%', height: 'auto' }}
-                      />
-                    </a>
-                  </figure>
-                </div>
-              ))}
             </div>
           </div>
         </section>
 
-        {/* ========== navigation section ========== */}
-        <ProjectDetailsNavigation />
+        {/* ========== projects section ========== */}
+        <section className="wrapper bg-light">
+          <div className="container py-14 py-md-16">
+            <div className="row mt-6">
+              <div className="col-xl-10 mx-auto">
+                <div className="projects-tiles">
+                  {projectList1.map((item) => (
+                    <ProjectCard1 {...item} key={item.id} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
 
       {/* ========== footer section ========== */}
