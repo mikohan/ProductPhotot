f721dc0b1fdabf3a2062efc699aaeea0c68bb64c
manhee <angara99@gmail.com>
2023-06-10 14:53:15 -0700

    Refactor player

--- pages/photo/projects/[slug].tsx
+++ pages/photo/projects/[slug].tsx
@@ -13,11 +13,13 @@ import NextLink from 'components/reuseable/links/NextLink';
 import ProjectDetailsContent from 'components/common/ProjectDetailsContent';
 import ProjectDetailsNavigation from 'components/common/ProjectDetailsNavigation';
 import Button from 'components/elements/NavbarButton';
+import Plyr from 'plyr-react';
 
 import { ProjectCard1 } from 'components/reuseable/project-cards';
 // -------- data -------- //
 import { gallery, projectList1 } from 'data/project';
 import { company } from 'data/company-info';
+const images = ['/img/photos/pp2.jpg', '/img/photos/pp3.jpg', '/img/photos/pp4.jpg', '/img/photos/pp5.jpg'];
 
 const ProjectDetails: NextPage = () => {
   // used for image lightbox
@@ -44,33 +46,92 @@ const ProjectDetails: NextPage = () => {
       </header>
 
       <main className="content-wrapper">
-        {/* ========== title section ========== */}
-        <section className="section-frame overflow-hidden">
-          <div className="wrapper bg-gray">
-            <div className="container py-13 py-md-17 text-center">
-              <div className="row">
-                <div className="col-lg-10 col-xxl-8 mx-auto">
-                  <h1 className="display-1 mb-1">Check out some of our awesome projects with creative ideas.</h1>
+        <section className="wrapper bg-soft-primary">
+          <div className="container pt-10 pb-19 pt-md-14 pb-md-22 text-center">
+            <div className="row">
+              <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <div className="post-header">
+                  <div className="post-category text-line">
+                    <NextLink title="Identity" href="#" className="hover" />
+                  </div>
+
+                  <h1 className="display-1 mb-3">Commodo Dolor Bibendum Parturient Cursus Mollis</h1>
+                  <p className="lead px-md-12 px-lg-12 px-xl-15 px-xxl-18">
+                    Integer posuere erat a ante venenatis dapibus posuere. Maecenas faucibus mollis interdum.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </section>
 
-        {/* ========== projects section ========== */}
-        <section className="wrapper bg-light">
-          <div className="container py-14 py-md-16">
-            <div className="row mt-6">
-              <div className="col-xl-10 mx-auto">
-                <div className="projects-tiles">
-                  {projectList1.map((item) => (
-                    <ProjectCard1 {...item} key={item.id} />
-                  ))}
-                </div>
+        <section className="wrapper bg-light wrapper-border">
+          <div className="container pb-14 pb-md-16">
+            <div className="row">
+              <div className="col-12">
+                <article className="mt-n21">
+                  <FigureImage width={2400} height={1640} src="/img/photos/pp1.jpg" className="rounded mb-8 mb-md-12" />
+
+                  <ProjectDetailsContent title="About the Project" />
+
+                  <div className="row mt-5 gx-md-6 gy-6">
+                    {images.map((item) => (
+                      <div className="item col-md-6" key={item}>
+                        <figure className="hover-scale rounded cursor-dark">
+                          <a href={item} data-glightbox data-gallery="project-1">
+                            <Image
+                              width={960}
+                              height={640}
+                              src={item}
+                              alt="demo"
+                              style={{ width: '100%', height: 'auto' }}
+                            />
+                          </a>
+                        </figure>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="row mt-5 gx-md-6 gy-6">
+                    <Plyr
+                      poster="/img/photos/pp2.jpg"
+                      options={{
+                        loadSprite: true,
+                        clickToPlay: true,
+                        quality: { default: 720, options: [4320, 2880, 2160, 1440, 1080, 720, 576, 480, 360, 240] }
+                      }}
+                      source={{ type: 'video', sources: [{ src: '6PfAEJIbm_A', provider: 'youtube' }] }}
+                    />
+                  </div>
+
+                  <div className="row mt-8 mt-md-12">
+                    <div className="col-md-10 offset-md-1 text-justify">
+                      <h2 className="mb-4">Quam Mollis Bibendum</h2>
+                      <p>
+                        Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non
+                        metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia
+                        bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Praesent commodo cursus
+                        magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus.
+                        Donec id elit non mi porta gravida at eget metus. Cras mattis consectetur purus sit amet
+                        fermentum.
+                      </p>
+
+                      <p>
+                        Aenean lacinia bibendum nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat
+                        porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Maecenas
+                        sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac,
+                        vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum
+                        etiam porta.
+                      </p>
+                    </div>
+                  </div>
+                </article>
               </div>
             </div>
           </div>
         </section>
+
+        {/* ========== navigation section ========== */}
+        <ProjectDetailsNavigation />
       </main>
 
       {/* ========== footer section ========== */}
